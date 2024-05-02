2cf7ae05fd9190299e2d830f7f848ca9bb190d2e
Kate Tsaruk <ipz224_tska@student.ztu.edu.ua>
2024-05-02 16:23:08 +0300

    Start of work on lab 5

---
+++ lab-5/task5/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LightNode
+{
+    public virtual string GetOuterHtml() { return ""; }
+    public virtual string GetInnerHtml() { return ""; }
+}
+
+class LightTextNode : LightNode
+{
+    private string _text;
+    public LightTextNode(string text)
+    {
+        _text = text;
+    }
+    public override string GetOuterHtml()
+    {
+        return _text;
+    }
+    public override string GetInnerHtml()
+    {
+        return _text;
+    }
+}
+
+class LightElementNode : LightNode
+{
+    private string _tagName;
+    private string _displayType;
+    private string _closingType;
+    private List<LightNode> _children;
+    private List<string> _cssClasses;
+
+    public LightElementNode(string tagName, string displayType, string closingType, List<string> cssClasses)
+    {
+        _tagName = tagName;
+        _displayType = displayType;
+        _closingType = closingType;
+        _cssClasses = cssClasses;
+        _children = new List<LightNode>();
+    }
+
+    public void AddChild(LightNode node)
+    {
+        _children.Add(node);
+    }
+
+    public override string GetOuterHtml()
+    {
+        string result = $"<{_tagName} class=\"{string.Join(" ", _cssClasses)}\" display=\"{_displayType}\" closing=\"{_closingType}\">\n";
+        foreach (var child in _children)
+        {
+            result += $"\t{child.GetOuterHtml()}\n";
+        }
+        if (_closingType == "closing")
+        {
+            result += $"</{_tagName}>";
+        }
+        return result;
+    }
+
+    public override string GetInnerHtml()
+    {
+        string result = "";
+        foreach (var child in _children)
+        {
+            result += child.GetInnerHtml();
+        }
+        return result;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        LightElementNode header = new LightElementNode("h1", "block", "closing", new List<string>());
+        LightTextNode headerText = new LightTextNode("Welcome to my page!");
+        header.AddChild(headerText);
+
+        LightElementNode table = new LightElementNode("table", "block", "closing", new List<string> { "styled-table" });
+
+        LightElementNode tableRow1 = new LightElementNode("tr", "block", "closing", new List<string>());
+        table.AddChild(tableRow1);
+
+        LightElementNode tableData1 = new LightElementNode("td", "inline", "closing", new List<string>());
+        LightTextNode dataText1 = new LightTextNode("Cell 1");
+        tableData1.AddChild(dataText1);
+        tableRow1.AddChild(tableData1);
+
+        LightElementNode tableData2 = new LightElementNode("td", "inline", "closing", new List<string>());
+        LightTextNode dataText2 = new LightTextNode("Cell 2");
+        tableData2.AddChild(dataText2);
+        tableRow1.AddChild(tableData2);
+
+        Console.WriteLine(header.GetOuterHtml());
+        Console.WriteLine(table.GetOuterHtml());
+    }
+}
